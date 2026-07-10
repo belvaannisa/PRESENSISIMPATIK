edd8931b1da9650ee248cbf14c6081de20773f43
Aiwnahaks <gsd.twins@gmail.com>
2026-07-10 02:44:09 +0000

    apiberhasil

--- app/Http/Middleware/VerifyApiKey.php
+++ app/Http/Middleware/VerifyApiKey.php
@@ -7,17 +7,19 @@ use Illuminate\Http\Request;
 
 class VerifyApiKey
 {
-    public function handle(Request $request, Closure $next)
-    {
-        if ($request->header('X-API-KEY') != env('API_KEY')) {
 
-            return response()->json([
-                'success'=>false,
-                'message'=>'Unauthorized'
-            ],401);
+public function handle(Request $request, Closure $next)
+{
+    if ($request->header('X-API-KEY') !== env('API_KEY')) {
 
-        }
+        return response()->json([
+            'success' => false,
+            'message' => 'Unauthorized'
+        ], 401);
 
-        return $next($request);
     }
+
+    return $next($request);
+}
+
 }
